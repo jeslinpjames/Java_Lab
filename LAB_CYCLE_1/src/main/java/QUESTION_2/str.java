6879b643cc7908dbb7ff10cba9439b5ce5f30f39
jeslinpjames <jeslinpjames007@gmail.com>
2023-07-07 10:05:01 +0530

    str class complete

---
+++ LAB_CYCLE_1/src/main/java/QUESTION_2/str.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package QUESTION_2;
+
+/**
+ *
+ * @author jeslinpjames
+ */
+public class str {
+    public void repeat(String s, int n){
+        for(int i =0;i<n;i++)
+            System.out.println(s);
+    }
+    
+}
