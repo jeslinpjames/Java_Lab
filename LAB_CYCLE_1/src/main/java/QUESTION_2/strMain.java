1014cb98260ec9c24042a2bbc9769fd8b8218737
jeslinpjames <jeslinpjames007@gmail.com>
2023-07-07 10:06:51 +0530

    strMain class added

---
+++ LAB_CYCLE_1/src/main/java/QUESTION_2/strMain.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package QUESTION_2;
+
+/**
+ *
+ * @author james
+ */
+public class strMain {
+     public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        String s = args[1];
+        str ob = new str();
+        ob.repeat(s, n);
+        
+    }
+    
+}
