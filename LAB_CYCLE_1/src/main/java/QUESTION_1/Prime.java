2d3ea614b2c39e7ea4255e79bf219717f1cf521f
jeslinpjames <jeslinpjames007@gmail.com>
2023-07-07 10:01:34 +0530

    Deleted everything and added Question1

--- LAB_CYCLE_1/src/main/java/QUESTION_1/Prime.java
+++ LAB_CYCLE_1/src/main/java/QUESTION_1/Prime.java
@@ -2,30 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package QUESTION_1;
 
 /**
  *
- * @author jeslinpjames
+ * @author jelsinpjames
  */
 public class Prime {
-    public static void main(String args[]){
-       int n = Integer.parseInt(args[0]);
-       Prime ob = new Prime();
-       boolean b = ob.check_prime(n);
-       int count =0;
-       int i = 1;
-       System.out.print("The First "+n+" prime numbers are : ");
-       while(count<n){
-           boolean ch = ob.check_prime(i);
-           if(ch){
-               count++;
-	       System.out.print(i+" ");
-           }
-           i++;
-       }       
-       System.out.println();
-    }
-    public boolean check_prime(int no){
+     public boolean check_prime(int no){
         int c =0;
         boolean b = false;
         for(int i =1;i<=no/2;i++){
@@ -36,4 +20,18 @@ public class Prime {
             b = true;
         return b;
     }
+     public void print_prime(int n){
+       int count =0;
+       int i = 1;
+       System.out.print("The First "+n+" prime numbers are : ");
+       while(count<n){
+           boolean ch = check_prime(i);
+           if(ch){
+               count++;
+	       System.out.print(i+" ");
+           }
+           i++;
+       }       
+       System.out.println();  
+     }
 }
---
+++ LAB_CYCLE_1/src/main/java/QUESTION_1/PrimeMain.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package QUESTION_1;
+
+/**
+ *
+ * @author jeslinpjames
+ */
+public class PrimeMain {
+    public static void main(String args[]){
+        int n = Integer.parseInt(args[0]);
+        Prime ob = new Prime();
+        ob.print_prime(n);        
+    }   
+}
