ac2a4e6767883c05db90e71e69669f57f7d11db6
jeslinpjames <jeslinpjames007@gmail.com>
2023-07-18 19:16:04 +0530

    Cycle 2

---
+++ LAB_CYCLE_2/src/main/java/com/mycompany/lab_cycle_2/LAB_CYCLE_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab_cycle_2;
+
+/**
+ *
+ * @author james
+ */
+public class LAB_CYCLE_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
