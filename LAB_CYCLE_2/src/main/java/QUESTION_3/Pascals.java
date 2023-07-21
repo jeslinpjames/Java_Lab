f7b89b18d8445db3a80787b9d8ae3a805f4e9fca
jeslinpjames <jeslinpjames007@gmail.com>
2023-07-22 05:15:42 +0530

    Finished Pascals

---
+++ LAB_CYCLE_2/src/main/java/QUESTION_3/Pascals.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package QUESTION_3;
+
+/**
+ *
+ * @author jeslinpjames
+ */
+public class Pascals {
+    private int len;
+    public Pascals(){
+        len=0;
+    }
+    public Pascals(int len){
+        this.len= len;
+    }
+    public int factorial(int n){
+        int f=1;
+        while(n>0){
+            f=f*n;
+            n=n-1;
+        }
+        return f;
+    }
+    public int nCr(int n, int r){
+        if (n==0)
+            return 1;
+        int a = factorial(n);
+        int b = factorial(r);
+        int c = factorial(n-r);
+        int ncr =a / (b * c);
+        return ncr;
+    }
+    public void printPascals(){
+        int z =len;
+        for(int i=0;i<len;i++){
+            for (int k =z;k>0;k--)
+                System.out.print(" ");
+            for(int j=0;j<=i;j++){
+                int ncr = nCr(i,j);
+                System.out.print(ncr+" ");
+            }
+            System.out.println();
+            z=z-1;
+        }
+    }
+}
---
+++ LAB_CYCLE_2/src/main/java/QUESTION_3/PascalsMain.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package QUESTION_3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jeslinpjames
+ */
+public class PascalsMain {
+    public static void main(String args[]){
+        System.out.print("Enter the number of rows required : ");
+        Scanner sc = new Scanner(System.in);
+        int len = sc .nextInt();
+        Pascals obj = new Pascals(len);
+        obj.printPascals();
+    }
+    
+}
