29f0159c6295cf57e1067c9b57b054df309a0d7c
jeslinpjames <jeslinpjames007@gmail.com>
2023-10-14 11:25:55 +0530

    updated to show which input is wrong

--- LAB_CYCLE_3/src/main/java/QUESTION_21/AWT.java
+++ LAB_CYCLE_3/src/main/java/QUESTION_21/AWT.java
@@ -10,23 +10,19 @@ package QUESTION_21;
  */
 import java.awt.*;
 import java.awt.event.*;
-import java.util.regex.*;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.util.regex.*;
 import javax.swing.*;
 
 public class AWT extends JFrame {
     private JLabel nameLabel, ageLabel, phoneLabel, emailLabel;
     private JTextField nameField, ageField, phoneField, emailField;
     private JButton submitButton;
+    private JLabel errorLabel;
 
     public AWT() {
         setTitle("Form Validation Example");
         setSize(400, 250);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new GridLayout(5, 2));
+        setLayout(new GridLayout(6, 2));
 
         nameLabel = new JLabel("Name:");
         ageLabel = new JLabel("Age:");
@@ -37,15 +33,18 @@ public class AWT extends JFrame {
         ageField = new JTextField();
         phoneField = new JTextField();
         emailField = new JTextField();
+        errorLabel = new JLabel("");
 
         submitButton = new JButton("Submit");
         submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (validateForm()) {
+                String error = validateForm();
+                if (error.isEmpty()) {
                     JOptionPane.showMessageDialog(null, "Form submitted successfully!");
+                    errorLabel.setText(""); // Clear any previous error message
                 } else {
-                    JOptionPane.showMessageDialog(null, "Invalid input. Please check the fields.");
+                    errorLabel.setText("Invalid input: " + error);
                 }
             }
         });
@@ -60,34 +59,36 @@ public class AWT extends JFrame {
         add(emailField);
         add(new JLabel()); // Placeholder
         add(submitButton);
+        add(errorLabel);
 
         setVisible(true);
     }
 
-    private boolean validateForm() {
+    private String validateForm() {
+        StringBuilder error = new StringBuilder();
+
         String name = nameField.getText();
         String age = ageField.getText();
         String phoneNumber = phoneField.getText();
         String email = emailField.getText();
 
-        
         if (name.isEmpty() || containsDigit(name)) {
-            return false;
+            error.append("Name is invalid. ");
         }
 
-        if (age.isEmpty() || !isNumeric(age)||(Integer.parseInt(age)>=130)) {
-            return false;
+        if (age.isEmpty() || !isNumeric(age) || Integer.parseInt(age) <= 0) {
+            error.append("Age is invalid. ");
         }
 
-       if (!isNumeric(phoneNumber) || phoneNumber.length() != 10) {
-            return false;
+        if (!isNumeric(phoneNumber) || phoneNumber.length() != 10) {
+            error.append("Phone number is invalid. ");
         }
 
-        if (!email.contains("@") || !email.contains(".")) {
-            return false;
-        }   
+        if (!isValidEmail(email)) {
+            error.append("Email is invalid.");
+        }
 
-        return true;
+        return error.toString();
     }
 
     public boolean containsDigit(String str) {
@@ -98,6 +99,7 @@ public class AWT extends JFrame {
         }
         return false;
     }
+
     public boolean isNumeric(String str) {
         try {
             Integer.parseInt(str);
@@ -106,6 +108,12 @@ public class AWT extends JFrame {
             return false;
         }
     }
+
+    public boolean isValidEmail(String email) {
+        String emailPattern = "^[A-Za-z0-9+_.-]+@(.+)$";
+        return email.matches(emailPattern);
+    }
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
